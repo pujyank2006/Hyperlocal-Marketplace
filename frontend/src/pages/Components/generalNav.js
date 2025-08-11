57b3e09d40752b719dfda2f57fb1157544e3b4c0
pujyank2006 <pujyank2006@gmail.com>
2025-08-11 22:38:14 +0530

    Added other components(headers)

---
+++ frontend/src/pages/Components/generalNav.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './generalNav.module.css';
+
+function GeneralNav() {
+  return (
+    <div className={styles.mainNav}>
+        <ul className={styles.lists}>
+            <li className={styles.eachList}>
+                <Link to="/">Home</Link>
+            </li>
+            <li className={styles.eachList}>
+                <Link to="/about">About</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default GeneralNav
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,12 +2,16 @@ import { toast } from 'react-toastify';
 
 export const handleSuccess = (msg) =>{
     toast.success(msg, {
-        position: 'top-right'
+        position: 'top-right',
+        theme: 'dark',
+        autoClose: 2000,
     })
 }
 
 export const handleError = (msg) =>{
     toast.error(msg, {
-        position: 'top-right'
+        position: 'top-right',
+        theme: 'dark',
+        autoClose: 2000,
     })
 }
