0c9e370715c0f0a4cb0e60ab01fdb93382bfdddc
pujyank2006 <pujyank2006@gmail.com>
2025-08-23 23:08:38 +0530

    Tweaked small changed

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 
 export const handleSuccess = (msg) =>{
     toast.success(msg, {
-        position: 'top-right',
+        position: 'top-center',
         theme: 'dark',
         autoClose: 2000,
     })
@@ -10,7 +10,7 @@ export const handleSuccess = (msg) =>{
 
 export const handleError = (msg) =>{
     toast.error(msg, {
-        position: 'top-right',
+        position: "top-center",
         theme: 'dark',
         autoClose: 2000,
     })
