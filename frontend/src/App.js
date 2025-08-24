ac8d3130c3bdae7eb074410f328e9b66318be991
pujyank2006 <pujyank2006@gmail.com>
2025-08-24 23:11:50 +0530

    Designed the account details popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Login from './pages/loginPage';
 import Home from './pages/homePage';
 import Signup from './pages/signupPage';
 import Dashboard from './pages/dashboard';
-
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
             <Route path = '/login' element = { <Login /> } />
             <Route path = '/signup' element = { <Signup /> } />
             <Route path = '/dashboard' element = { <Dashboard /> } />
+            <Route path = '/profile' element = { <Profile /> } />
         </Routes>
     </div>
   )
---
+++ frontend/src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function profile() {
+  return (
+    <div>
+      In profile.
+    </div>
+  )
+}
+
+export default profile
