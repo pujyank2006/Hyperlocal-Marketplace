5398cd8975d3e10630c168e757155d8c1c0107a1
pujyank2006 <pujyank2006@gmail.com>
2025-08-25 21:47:16 +0530

    just did very few things

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-function profile() {
+import logo from "./assets/favicon.png";
+import title2 from "./assets/Title2.png";
+import account from "./assets/accountCircle.svg";
+import styles from "../styles/dashboard.module.css";
+import AccountDetails from './Components/accountDetails';
+
+function Profile() {
+  const [isAccountOpen, setAccountOpen] = useState(false);
+
   return (
-    <div>
-      In profile.
+
+    <div className={styles.mainHeader}>
+      <Link to="/">
+        <img alt='pic' src={logo} width='40px' height='40px' />
+        &nbsp;
+        <img alt='pic' src={title2} className={styles.title} />
+      </Link>
+      <button className={styles.button} onClick={() => setAccountOpen(!isAccountOpen)}><img alt='pic' src={account} width='40px' height='40px' /></button>
+      <AccountDetails open={isAccountOpen} onClose={() => setAccountOpen(false)}></AccountDetails>
     </div>
   )
 }
 
-export default profile
+export default Profile
