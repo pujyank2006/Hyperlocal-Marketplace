71274913fe7b9723e2b89c8712685e85f65270dd
pujyank2006 <pujyank2006@gmail.com>
2025-09-01 22:19:58 +0530

    Added address edit/add option

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,15 +1,54 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 import logo from "./assets/favicon.png";
 import title2 from "./assets/Title2.png";
 import account from "./assets/accountCircle.svg";
 import styles from "../styles/profile.module.css";
 import AccountDetails from './Components/accountDetails';
+import { handleError, handleSuccess } from '../utils';
 
 function Profile() {
   const [isAccountOpen, setAccountOpen] = useState(false);
   const [user, setUser] = useState(null);
+  const [showInput, setShowInput] = useState(false);
+  const [address, setAddress] = useState("");
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  async function handleAddress(req, res) {
+    if(address.trim() === ""){
+      handleError("empty address cannot be updated!");
+      return;
+    }
+    const token = localStorage.getItem('token');
+    try {
+      const url = "http://localhost:9000/api/addDetails";
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({ address })
+      })
+      const result = await response.json();
+      const { message, success, error } = result;
+      if (success) {
+        handleSuccess(message);
+      } else if (error) {
+        const details = error.details[0].message;
+        handleError(details);
+      } else if (!success) {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -17,6 +56,7 @@ function Profile() {
     if (!token) {
       return;
     }
+
     const url = "http://localhost:9000/api/loggedInUser";
     fetch(url, {
       method: 'GET',
@@ -29,6 +69,7 @@ function Profile() {
       .then((data) => setUser(data))
       .catch(err => console.error("Error fetching user: ", err));
   }, []);
+
 
   if (!user) {
     return <p>Loading...</p>
@@ -49,14 +90,21 @@ function Profile() {
       <div className={styles.information}>
         <div className={styles.detailsPage}>
           <div className={styles.personalDetails}>
-            Image will be set
+            <p>Image will be set <button>edit</button></p>
             <h1>{user.name}</h1>
             <p>{user.email}</p>
             <p>{user.phone}</p>
           </div>
           <div className={styles.locationDetails}>
-            <label>Add your full address: </label>
-            <input type='text' placeholder='Full address' />
+            {!showInput ? (
+              <p>Add address <button onClick={() => setShowInput(true)}>Add</button></p>
+            ) : (
+              <>
+                <label>Add your full address: </label>
+                <input type='text' placeholder='Full address' name="address" onChange={handleAddressChange} />
+                <button onClick={handleAddress}>Confirm</button>
+              </>
+            )}
             <h2>{user.area}, {user.city}</h2>
             <h2>{user.pincode}</h2>
           </div>
@@ -65,6 +113,7 @@ function Profile() {
           Currently empty
         </div>
       </div>
+      <ToastContainer />
     </>
   )
 }
